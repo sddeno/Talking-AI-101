138446a5e7232d4b967b2d4a39e24edb144ee471
Shubham Deshmukh <shubhamdeshmukh69@gmail.com>
2023-06-21 18:09:50 +0530

    Initial Commit

---
+++ Talking AI/Talking_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Talking_AIApp.swift
+//  Talking AI
+//
+//  Created by Shubham Deshmukh on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Talking_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
